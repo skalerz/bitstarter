9cf668822e222769e3204f8b6500facc492039b9
Ubuntu <ubuntu@ip-172-31-2-7.eu-west-1.compute.internal>
2013-07-10 21:21:27 +0000

    ajout d'index.html et modif pour lecture de l'index par le js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var content = fs.readFileSync('./index.html');
+
+var buffer = new Buffer(content,"utf-8");
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
